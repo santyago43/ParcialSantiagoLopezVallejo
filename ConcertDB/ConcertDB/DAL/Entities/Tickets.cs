b082e83b3ec3844a6930dc68501651ab25ac06fc
SANTIAGO LOPEZ VALLEJO <santiagolopez305466@correo.itm.edu.co>
2023-05-21 14:33:10 -0500

    Base Project

---
+++ ConcertDB/ConcertDB/DAL/Entities/Tickets.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ConcertDB.DAL.Entities
+{
+    public class Tickets
+    {
+        [Display(Name = "Tickets")]
+        [Required]
+        public Guid Id { get; set; }
+
+        public DateOnly? UseDate { get; set; }
+
+        public Boolean IsUsed { get; set; }
+
+        public String? EntranceGate { get; set; }
+
+    }
+}
